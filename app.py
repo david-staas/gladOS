57fa402e72949af39f3807ff3a86979dd6e692b9
David Staas <david.staas@gmail.com>
2022-05-10 19:21:58 -0700

    Integrating 9685 code with Flask app

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from markupsafe import escape
 from flask import render_template
+import PCA9685
+import time
 
 app = Flask(__name__)
 v = {}
+pwm = PCA9685(0x40, debug=False)
+pwm.setPWMFreq(50)
+
 @app.route("/")
 def main():
     return render_template('/home.html', v=v)
@@ -11,5 +16,8 @@ def main():
 @app.route("/api/motor/<motor>/<location>")
 def move_base(motor, location):
     s = f"moving motor {escape(motor)} to {escape(location)}"
+    if motor == "base":
+        pwm.setServoPulse(10, 1250)
+        time.sleep(0.02)
     print(s)
     return s
